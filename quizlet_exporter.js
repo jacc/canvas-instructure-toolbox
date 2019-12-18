805da4960897da31bc25985d178e314c09ddf4a7
Jack LaFond <jack.lafond@icloud.com>
2019-12-18 17:29:46 -0500

    Added attempt stats + not showing incorrect log if there are no incorrect answers

--- quizlet_exporter.js
+++ quizlet_exporter.js
@@ -1,5 +1,7 @@
-var correct = "",
-    incorrect = "";
+var wrongs = "",
+    rights = "",
+	wrongcounter = 0,
+	rightcounter = 0
 $('.display_question').each(function(){
     var incorrect = $(this).hasClass('incorrect'),
         question_text = $(this).find('.question_text').text().trim(),
@@ -7,11 +9,16 @@ $('.display_question').each(function(){
         krunked = `${question_text} - ${incorrect ? `NOT ${answer_text}` : answer_text}\n`
     if(incorrect){
         wrongs += krunked
+		wrongcounter += 1
     } else {
         rights += krunked
+		rightcounter += 1
     }
 });
 
 console.log(`================\njacc's Canvas to Quizlet Exporter\nCopyright (c) 2019 Professional Yadi Krunkers\nOG version by @strix on GitHub\nFor educational use only (literally)\n================`)
-console.log(`================\nCorrect Questions / Answers\n${correct}================`)
-console.log(`================\nIncorrect Questions / Answers\n${incorrect}================`)
+console.log(`================\nAttempt stats:\nCorrect answers: ${rightcounter}\nIncorrect answers: ${wrongcounter}\n================`)
+console.log(`================\nCorrect Questions / Answers ${rights}\n================`)
+if (wrongcounter > 0) {
+    console.log(`================\nIncorrect Questions / Answers ${wrongs}\n================`)
+}
